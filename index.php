0c1319652cd6503d6931a925b298a098fe1601e0
Niushaaaa <117599554+Niushaaaa@users.noreply.github.com>
2023-05-14 14:08:13 -0700

    اضافه کردن دکمه برای نام و خانوادگی و معدل با insert

--- index.php
+++ index.php
@@ -8,10 +8,20 @@
 <body>
 <?php
 include "connection.php";
-if (isset($_POST['submit1'])){
-    $id=$_POST['txt_id'];
-    $class=$_POST['txt_class'];
-    $ins_sql="INSERT INTO class(id,class)VALUES ('$id','$class')";
+if (isset($_POST['submit1'])) {
+    $id = $_POST['txt_id'];
+    $class = $_POST['txt_class'];
+    $ins_sql = "INSERT INTO class(id,class)VALUES ('$id','$class')";
+    $ins_sql_pre = $db->prepare($ins_sql);
+    $ins_sql_pre->execute();
+}
+if (isset($_POST['submit2'])) {
+    $id = $_POST['id'];
+    $class = $_POST['class'];
+    $name = $_POST['name'];
+    $family = $_POST['family'];
+    $ave = $_POST['ave'];
+    $ins_sql = "INSERT INTO student(id,class,name,family,ave) VALUES ('$id','$class','$name','$family','$ave')";
     $ins_sql_pre=$db->prepare($ins_sql);
     $ins_sql_pre->execute();
 }
