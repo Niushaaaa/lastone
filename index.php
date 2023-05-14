d32d86c275db0da1f234b402ab21c353747db452
Niushaaaa <117599554+Niushaaaa@users.noreply.github.com>
2023-05-14 13:59:24 -0700

    لینک کردن فایل connectionبهindex و اضافه کردن دکمه submit ,id,classبرای اضافه کردن مقدار یا همان insert

--- index.php
+++ index.php
@@ -5,4 +5,16 @@
     <title>school database sample1</title>
     <link href="css/style.css" rel="stylesheet">
 </head>
+<body>
+<?php
+include "connection.php";
+if (isset($_POST['submit1'])){
+    $id=$_POST['txt_id'];
+    $class=$_POST['txt_class'];
+    $ins_sql="INSERT INTO class(id,class)VALUES ('$id','$class')";
+    $ins_sql_pre=$db->prepare($ins_sql);
+    $ins_sql_pre->execute();
+}
+?>
+</body>
 </html>
