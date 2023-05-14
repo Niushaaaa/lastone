83e97e8dece0a7755ab63b6fed9259eb05faff14
Niushaaaa <117599554+Niushaaaa@users.noreply.github.com>
2023-05-14 13:25:48 -0700

    وارد کردن مشخصات دیتابیس و تعریف کردن زبان فارسی برای اسمها

--- connection.php
+++ connection.php
@@ -1,1 +1,9 @@
 <?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "lastone";
+$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$sql = "SET NAMES 'utf-8'";
+$result=$db->prepare($sql);
+$result->execute();
