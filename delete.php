67b25c18d6c034bf526ad9643c95a51ffdd7df11
Niushaaaa <117599554+Niushaaaa@users.noreply.github.com>
2023-05-14 22:47:08 -0700

    کووری دادن برای دیلیت

--- delete.php
+++ delete.php
@@ -1,1 +1,21 @@
-<?php
+<?php session_start();
+ob_start();
+include "connection.php";
+if (isset($_GET['id']) && isset($_GET['page'])) {
+    $id = $_GET['id'];
+    $page = $_GET['page'];
+    switch ($page) {
+        case 1;
+            $query = "DELETE FROM class WHERE name=" . $id;
+            $del = $db->prepare($query);
+            $del->execute();
+            header("location:index.php");
+            break;
+        case 2:
+            $query = "DELETE FROM student WHERE id=" . $id;
+            $del = $db->prepare($query);
+            $del->execute();
+            header("location:index.php");
+            break;
+    }
+}
